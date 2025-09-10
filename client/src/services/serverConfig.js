58925e27c08b55b226158e876fe3053cb4ec90c1
VaibhavPatil01 <vaibhavvpatil132@gmail.com>
2025-09-10 16:55:19 +0530

    Testing

--- client/src/services/serverConfig.js
+++ client/src/services/serverConfig.js
@@ -1,3 +1,3 @@
 export const LATENCY_IN_SECONDS = 1;
 
-export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || 'http://localhost:3000';
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || 'http://localhost:3000'; 
--- server/server.js
+++ server/server.js
@@ -34,14 +34,18 @@ await connectDB();
 const app = express();
 
 // CORS Options
-const corsOptions = {
-  origin: process.env.CLIENT_BASE_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-};
+// const corsOptions = {
+//   origin: process.env.CLIENT_BASE_URL || 'http://localhost:5173',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// };
 
 // Middlewares
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
